2d340ff6b416b0108d5fa560f4b5c00ccf3a45a4
Roma Potsko <roma.potsko@gmail.com>
2015-10-13 11:48:46 +0300

    список проектов

--- frontend/javascripts/app.js
+++ frontend/javascripts/app.js
@@ -25,5 +25,24 @@ app.run(['$rootScope', '$state', function($rootScope, $state) {
 		}
 	});
 }]);
+app.factory('dataFactory', function ($q,$http) {
+	return	{
+		getMongoData: getMongoData
+	};
+			function getMongoData () {
+			var deferred = $q.defer(),
+				httpPromise = $http.get('/api/projects');
+
+			httpPromise.success(function (projects) {
+				deferred.resolve(projects);
+
+			})
+				.error(function (error) {
+					console.error('Error: ' + error);
+				});
+
+			return deferred.promise;
+		}
+});
 
 module.exports = app;
