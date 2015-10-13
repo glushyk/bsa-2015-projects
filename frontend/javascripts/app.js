ad62bb6a9e642deeeda2bb28e70365ba047649f9
Vadim Avdeev <v.avdeiev@gmail.com>
2015-10-13 13:46:07 +0300

    Revert "Projects list"

--- frontend/javascripts/app.js
+++ frontend/javascripts/app.js
@@ -25,24 +25,5 @@ app.run(['$rootScope', '$state', function($rootScope, $state) {
 		}
 	});
 }]);
-app.factory('dataFactory', function ($q,$http) {
-	return	{
-		getMongoData: getMongoData
-	};
-			function getMongoData () {
-			var deferred = $q.defer(),
-				httpPromise = $http.get('/api/projects');
-
-			httpPromise.success(function (projects) {
-				deferred.resolve(projects);
-
-			})
-				.error(function (error) {
-					console.error('Error: ' + error);
-				});
-
-			return deferred.promise;
-		}
-});
 
 module.exports = app;
