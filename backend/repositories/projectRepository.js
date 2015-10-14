fc06decc694763b92ee33d7d206b0634fdc21b15
Roma Potsko <roma.potsko@gmail.com>
2015-10-14 18:06:32 +0300

    add User and Technology

--- backend/repositories/projectRepository.js
+++ backend/repositories/projectRepository.js
@@ -1,6 +1,8 @@
 var connection = require('../db/dbconnect');
 var Repository = require('./generalRepository');
 var Project = require('../schemas/projectSchema');
+var User  = require('../schemas/userSchema');
+var Technology  = require('../schemas/techSchema');
 
 function ProjectRepository() {
 	Repository.prototype.constructor.call(this);
