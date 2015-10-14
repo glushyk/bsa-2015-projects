580ae7f4ef24c04944cd6e753eb193c95f0181be
Roma Potsko <roma.potsko@gmail.com>
2015-10-14 18:06:52 +0300

    update projectSchema

--- backend/schemas/projectSchema.js
+++ backend/schemas/projectSchema.js
@@ -2,7 +2,16 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var projectSchema = new Schema({
-	name: String
+	_id : Schema.Types.ObjectId,
+	name: String,
+	description : String,
+	begin : { type: Date, default: Date.now },
+	end : Date,
+	users : [ {type : mongoose.Schema.ObjectId, ref : 'User'} ],
+	tags : [],
+	technologies : [ {type : mongoose.Schema.ObjectId, ref : 'Technology'} ],
+	stage : String,
+	screenshots : []
 });
 
 module.exports = mongoose.model('Project', projectSchema);
